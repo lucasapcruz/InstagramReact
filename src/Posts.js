902d27ed9fb16fe4d1e58d8bef18c1ecd0b9f8ca
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-14 23:09:19 -0300

    Add Post component

--- src/Posts.js
+++ src/Posts.js
@@ -1,77 +1,48 @@
-export default function Posts(){
-    return(
+function Post(props) {
+    return (
+        <div class="post">
+                <div class="topo">
+                    <div class="usuario">
+                        <img src={props.userimg} />
+                        {props.username}
+                    </div>
+                    <div class="acoes">
+                        <ion-icon name="ellipsis-horizontal"></ion-icon>
+                    </div>
+                </div>
+
+                <div class="conteudo">
+                    <img src={props.postimg} />
+                </div>
+
+                <div class="fundo">
+                    <div class="acoes">
+                        <div>
+                            <ion-icon name="heart-outline"></ion-icon>
+                            <ion-icon name="chatbubble-outline"></ion-icon>
+                            <ion-icon name="paper-plane-outline"></ion-icon>
+                        </div>
+                        <div>
+                            <ion-icon name="bookmark-outline"></ion-icon>
+                        </div>
+                    </div>
+
+                    <div class="curtidas">
+                        <img src={props.likesimg} />
+                        <div class="texto">
+                            Curtido por <strong>{props.likesHighlight}</strong> e <strong>outras {props.likesCount} pessoas</strong>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default function Posts() {
+    return (
         <div class="posts">
-        <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                    <img src="img/meowed.svg" />
-                    meowed
-                </div>
-                <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-
-            <div class="conteudo">
-                <img src="img/gato-telefone.svg" />
-            </div>
-
-            <div class="fundo">
-                <div class="acoes">
-                    <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="curtidas">
-                    <img src="img/respondeai.svg" />
-                    <div class="texto">
-                        Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                    </div>
-                </div>
-            </div>
+            <Post userimg="img/meowed.svg" username="meowed" postimg="img/gato-telefone.svg" likesimg="img/respondeai.svg" likesHighlight="respondeai" likesCount="101.523"/>
+            <Post userimg="img/barked.svg" username="barked" postimg="img/dog.svg" likesimg="img/adorable_animals.svg" likesHighlight="adorable_animals" likesCount="99.159"/>
         </div>
-
-        <div class="post">
-            <div class="topo">
-                <div class="usuario">
-                    <img src="img/barked.svg" />
-                    barked
-                </div>
-                <div class="acoes">
-                    <ion-icon name="ellipsis-horizontal"></ion-icon>
-                </div>
-            </div>
-
-            <div class="conteudo">
-                <img src="img/dog.svg" />
-            </div>
-
-            <div class="fundo">
-                <div class="acoes">
-                    <div>
-                        <ion-icon name="heart-outline"></ion-icon>
-                        <ion-icon name="chatbubble-outline"></ion-icon>
-                        <ion-icon name="paper-plane-outline"></ion-icon>
-                    </div>
-                    <div>
-                        <ion-icon name="bookmark-outline"></ion-icon>
-                    </div>
-                </div>
-
-                <div class="curtidas">
-                    <img src="img/adorable_animals.svg" />
-                    <div class="texto">
-                        Curtido por <strong>adorable_animals</strong> e <strong>outras 99.159 pessoas</strong>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     )
 }
