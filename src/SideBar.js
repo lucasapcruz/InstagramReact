c3c58542a48b445cc5fabafccdd3fb275aa58724
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-14 22:45:02 -0300

    Add Sugestoes and Posts components

--- src/SideBar.js
+++ src/SideBar.js
@@ -1,3 +1,4 @@
+import Sugestoes from "./Sugestoes"
 import Usuario from "./Usuario"
 
 export default function SideBar(){
@@ -5,72 +6,7 @@ export default function SideBar(){
         <div class="sidebar">
         <Usuario/>
 
-        <div class="sugestoes">
-          <div class="titulo">
-            Sugestões para você
-            <div>Ver tudo</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="img/bad.vibes.memes.svg" />
-              <div class="texto">
-                <div class="nome">bad.vibes.memes</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="img/chibirdart.svg" />
-              <div class="texto">
-                <div class="nome">chibirdart</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="img/razoesparaacreditar.svg" />
-              <div class="texto">
-                <div class="nome">razoesparaacreditar</div>
-                <div class="razao">Novo no Instagram</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="img/adorable_animals.svg" />
-              <div class="texto">
-                <div class="nome">adorable_animals</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-
-          <div class="sugestao">
-            <div class="usuario">
-              <img src="img/smallcutecats.svg" />
-              <div class="texto">
-                <div class="nome">smallcutecats</div>
-                <div class="razao">Segue você</div>
-              </div>
-            </div>
-
-            <div class="seguir">Seguir</div>
-          </div>
-        </div>
+        <Sugestoes/>
 
         <div class="links">
           Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes •
