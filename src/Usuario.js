d278c9b469e1a9c1de800d29c810b5c91afc8ddf
Lucas Cruz <lucas.apcruz@gmail.com>
2022-09-15 21:27:09 -0300

    Add change user name feature

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,12 +1,17 @@
+import { useState } from "react"
+
 export default function Usuario(){
+
+    const [userName, setUserName] = useState("Catana")
+
     return(
         <div class="usuario">
           <img src="img/catanacomics.svg" />
           <div class="texto">
             <strong>catanacomics</strong>
             <span>
-              Catana
-              <ion-icon name="pencil"></ion-icon>
+              {userName}
+              <ion-icon name="pencil" onClick={() => setUserName(prompt("Digite um nome de usuário:"))}></ion-icon>
             </span>
           </div>
         </div>
